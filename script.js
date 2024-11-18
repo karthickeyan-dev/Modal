ab71f167d843b07d6d60fce40a23806e5fb5ca26
karthickeyan <jkarthickeyan24@gmail.com>
2024-11-18 10:18:39 +0530

    Added variables to select elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnOpenModel = document.querySelectorAll(".show-modal");
